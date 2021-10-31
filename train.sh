8e38dc7a30644da0866843d71270154a5e520d96
Ani Chen <chenanni02@gmail.com>
2021-10-31 00:16:20 -0400

    Add python path

--- train.sh
+++ train.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
 NAME="aclImdbSimple_pretrained_mixed"
-OUT="../temp/$NAME"
+OUT="temp/$NAME"
 DATASET="aclImdb_tok"
 
 # # Preprocess
-# python3 ../scripts/preprocessing.py --train "../raw_datasets/aclImdb_tok/train.txt" \
+# python3 scripts/preprocessing.py --train "../raw_datasets/aclImdb_tok/train.txt" \
 #               --valid "../raw_datasets/aclImdb_tok/test.txt" \
 #               --test "../raw_datasets/aclImdb_tok/test.txt" \
 #               --unlabel "../raw_datasets/aclImdb_tok/test.txt" \
 #               --dataset ${DATASET} \
 #               -o ${OUT}/
 # echo "loading pretrained vectors..."
-# python3 ../scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+# python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
 #             --data_name "aclImdb_tok" \
 #             --pretrained_embeddings "../pretrained_embeddings/vectors_aclImdb.txt"
 
-# echo "preprocessing finished"
+# echo "preprocessing finished" 
 
-python3 ../scripts/main.py --data_folder=${OUT} \
+export PYTHONPATH="$PWD"
+python3 scripts/main.py --data_folder=${OUT} \
               --dataset=${DATASET} \
               --random_seed=10 \
               --batch_size=1 \
