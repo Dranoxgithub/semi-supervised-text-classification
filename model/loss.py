97a8fce9d6838120b35a6cac5df88f1ec9745010
Ani Chen <chenanni02@gmail.com>
2021-11-04 13:31:41 -0400

    Fix autograd for at

--- model/loss.py
+++ model/loss.py
@@ -5,21 +5,21 @@ from torch.autograd import Variable
 
 def at_loss(input_dict, custom_embedding, custom_LSTM, custom_classifier, X, Y, at_epsilon=5.0):
     criterion = nn.NLLLoss()
-    embedded = custom_embedding(X) # sent_len * bsz * embedding_dim
-    embedded.retain_grad()
+
+    embedded = custom_embedding(X).detach().requires_grad_(True) # sent_len * bsz * embedding_dim
     lstm_out, state = custom_LSTM(embedded, input_dict)
-    clf_out = custom_classifier(lstm_out)
-    logits = F.log_softmax(clf_out, dim=-1)
-    loss = criterion(logits, Y)
+    logit = custom_classifier(lstm_out)
+    log_softmax = F.log_softmax(logit, dim=-1)
+    loss = criterion(log_softmax, Y)
     loss.backward()
 
     g = normalize_matrix(embedded.grad.detach())
     pert_embedded = embedded + at_epsilon * g
     lstm_out, state = custom_LSTM(pert_embedded, input_dict)
-    clf_out = custom_classifier(lstm_out)
-    logits = F.log_softmax(clf_out, dim=-1)
+    logit = custom_classifier(lstm_out)
+    log_softmax = F.log_softmax(logit, dim=-1)
 
-    return criterion(logits, Y)
+    return criterion(log_softmax, Y)
 def normalize_matrix(matrix):
     # sent_len * bsz * embedding_dim
     matrix = torch.permute(matrix, [1, 0, 2])  # bsz * sent_len * embedding_dim
