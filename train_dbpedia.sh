6e62e7a4b5230271f6578c1d0b4115aaeae5f637
Waltwang441 <walter.wang@duke.edu>
2021-11-13 16:00:02 -0500

    update elec and dbpedia

---
+++ train_dbpedia.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+NAME="dbpedia_pretrained_mixed"
+OUT="temp/$NAME"
+DATASET="dbpedia"
+#'''
+#For Elec, AGNews and DBpedia, use lines below to download pretrained embeddings
+#'''
+#wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+#unzip crawl-300d-2M.vec.zip
+#mv crawl-300d-2M.vec pretrained_embeddings/
+
+#unzip tar.gz file
+tar -xvf raw_datasets/dbpedia/train.tar.gz
+mv train.txt raw_datasets/dbpedia
+tar -xvf raw_datasets/dbpedia/test.tar.gz
+mv test.txt raw_datasets/dbpedia
+
+# Preprocess
+python3 scripts/preprocessing.py --train "raw_datasets/dbpedia/train.txt" \
+               --valid "raw_datasets/dbpedia/test.txt" \
+               --test "raw_datasets/dbpedia/test.txt" \
+               --unlabel "raw_datasets/dbpedia/train.txt" \
+               --dataset ${DATASET} \
+               -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+             --data_name "dbpedia" \
+             --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
+echo "preprocessing finished"
+
+export PYTHONPATH="$PWD"
+python3 scripts/main.py --data_folder=${OUT} \
+              --dataset_name=${DATASET} \
+              --random_seed=50 \
+              --words_per_batch=5000 \
+              --num_epochs=50 \
+              --eval_freq=100 \
+              --num_labels=4 \
+              --enable_logging \
+              --logging_freq \
+              --use_CE \
+              --use_AT \
+              --use_EM \
+              --use_VAT
---
+++ train_elec.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+NAME="elec_pretrained_mixed"
+OUT="temp/$NAME"
+DATASET="elec"
+#'''
+#For Elec, AGNews and DBpedia, use lines below to download pretrained embeddings
+#'''
+#wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+#unzip crawl-300d-2M.vec.zip
+
+#unzip tar.gz file
+tar -xvf raw_datasets/elec/unlabel.tar.gz
+mv unlabel.txt raw_datasets/elec
+
+# Preprocess
+python3 scripts/preprocessing.py --train "raw_datasets/elec/train.txt" \
+               --valid "raw_datasets/elec/test.txt" \
+               --test "raw_datasets/elec/test.txt" \
+               --unlabel "raw_datasets/elec/unlabel.txt" \
+               --dataset ${DATASET} \
+               -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+             --data_name "elec" \
+             --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
+echo "preprocessing finished"
+
+export PYTHONPATH="$PWD"
+python3 scripts/main.py --data_folder=${OUT} \
+              --dataset_name=${DATASET} \
+              --random_seed=50 \
+              --words_per_batch=5000 \
+              --num_epochs=50 \
+              --eval_freq=100 \
+              --num_labels=4 \
+              --enable_logging \
+              --logging_freq \
+              --use_CE \
+              --use_AT \
+              --use_EM \
+              --use_VAT
