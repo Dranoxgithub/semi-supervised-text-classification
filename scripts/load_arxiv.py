c36a9862dfa38b49527181263029b846a876f884
Waltwang441 <walter.wang@duke.edu>
2021-11-13 15:43:09 -0500

    Adding train scripts and data

--- scripts/load_arxiv.py
+++ scripts/load_arxiv.py
@@ -1,4 +1,56 @@
 import datasets
-from datasets import load_dataset
-datasets_list = datasets.load_dataset("arxiv_dataset", data_dir="../raw_datasets/arxiv", ignore_verifications=True)
-print(datasets_list['train'][0])
+from gensim.models import Word2Vec
+import os
+import random
+
+path = "../raw_datasets/arxiv"
+if not os.path.exists(path):
+    os.mkdir(path)
+
+arxiv = datasets.load_dataset("arxiv_dataset", data_dir=path, ignore_verifications=True)
+arxiv_corpus = arxiv['train']['abstract']
+arxiv_label = arxiv['train']['categories']
+corpus_len = len(arxiv_corpus)  # should be 1971283
+
+train_path = path + "/train.txt"
+test_path = path + "/test.txt"
+corpus_path = path + "/corpus.txt"
+embedding_path = "../pretrained_embeddings/arxiv_pretrained.txt"
+
+
+test_size = 443000
+train_size = 664000
+zipped = list(zip(arxiv_corpus, arxiv_label))
+random.shuffle(zipped)
+shuffled_corpus, shuffled_label = zip(*zipped)
+
+print('start')
+with open(train_path, 'w') as file:
+    counter = 0
+    for i in range(train_size):
+        label = shuffled_label[i]
+        text = shuffled_corpus[i].lower()
+        text = text.replace("\n", " ")
+        temp = label + "\t" + "%s\n" % text
+        file.write(temp)
+
+with open(test_path, 'w') as file:
+    for i in range(train_size, train_size + test_size):
+        label = shuffled_label[i]
+        text = shuffled_corpus[i].lower()
+        text = text.replace("\n", " ")
+        temp = label + "\t" + "%s\n" % text
+        file.write(temp)
+
+# still train embeddings on the whole corpus
+with open(corpus_path, 'w') as file:
+    for lines in arxiv_corpus:
+        file.write(lines.lower())
+
+print('next')
+model = Word2Vec(corpus_file=corpus_path, vector_size=300)
+word_vector = model.wv
+del model
+word_vector.save_word2vec_format(embedding_path)
+print('done')
+
---
+++ train_arxiv.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+NAME="arxiv_pretrained_mixed"
+OUT="temp/$NAME"
+DATASET="arxiv"
+
+# run the following line to get raw data and load embeddings
+# need to manually upload kaggle.json to current directory first; the following lines will take care of it
+pip install kaggle
+mkdir ~/.kaggle
+mv ./kaggle.json ~/.kaggle/
+chmod 600 ~/.kaggle/kaggle.json
+mkdir raw_datasets/arxiv
+kaggle datasets download Cornell-University/arxiv
+unzip arxiv.zip
+mv arxiv-metadata-oai-snapshot.json raw_datasets/arxiv
+python3 scripts/load_arxiv.py
+
+# Preprocess
+python3 scripts/preprocessing.py --train "raw_datasets/arxiv/train.txt" \
+               --valid "raw_datasets/arxiv/test.txt" \
+               --test "raw_datasets/arxiv/test.txt" \
+               --unlabel "raw_datasets/arxiv/unlabel.txt" \
+               --dataset ${DATASET} \
+               -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+             --data_name "arxiv" \
+             --pretrained_embeddings "pretrained_embeddings/arxiv_pretrained.txt"
+echo "preprocessing finished"
+
+export PYTHONPATH="$PWD"
+python3 scripts/main.py --data_folder=${OUT} \
+              --dataset_name=${DATASET} \
+              --random_seed=10 \
+              --words_per_batch=10000 \
+              --num_epochs=10 \
+              --eval_freq=100 \
+              --use_AT \
+              --use_CE \
+              --use_EM \
+              --use_VAT \
+              --num_labels=4
