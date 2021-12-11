5c1c20c6c3d244e8c7eb1220aca25b1f7c42e069
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2021-12-11 16:16:25 -0500

    Update train_aclImdb.sh

--- train_aclImdb.sh
+++ train_aclImdb.sh
@@ -5,17 +5,17 @@ OUT="temp/$NAME"
 DATASET="aclImdb_tok"
 
 # Preprocess
-#python3 scripts/preprocessing.py --train "raw_datasets/aclImdb_tok/train.txt" \
-#              --valid "raw_datasets/aclImdb_tok/test.txt" \
-#              --test "raw_datasets/aclImdb_tok/test.txt" \
-#              --unlabel "raw_datasets/aclImdb_tok/test.txt" \
-#              --dataset ${DATASET} \
-#              -o ${OUT}/
-#echo "loading pretrained vectors..."
-#python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
-#            --data_name "aclImdb_tok" \
-#            --pretrained_embeddings "pretrained_embeddings/vectors_aclImdb.txt"
-#echo "preprocessing finished"
+python3 scripts/preprocessing.py --train "raw_datasets/aclImdb_tok/train.txt" \
+             --valid "raw_datasets/aclImdb_tok/test.txt" \
+             --test "raw_datasets/aclImdb_tok/test.txt" \
+             --unlabel "raw_datasets/aclImdb_tok/test.txt" \
+             --dataset ${DATASET} \
+             -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+           --data_name "aclImdb_tok" \
+           --pretrained_embeddings "pretrained_embeddings/vectors_aclImdb.txt"
+echo "preprocessing finished"
 
 
 export PYTHONPATH="$PWD"
