67a7f842be8212cf9b5e627ae884f33e7d34563a
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2021-12-11 16:20:15 -0500

    Update train_gap.sh

--- train_gap.sh
+++ train_gap.sh
@@ -4,18 +4,23 @@ NAME="gap"
 OUT="temp/$NAME"
 DATASET="gap_processed"
 
+# get pretrained embeddings
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+unzip crawl-300d-2M.vec.zip
+mv crawl-300d-2M.vec pretrained_embeddings/
+
 # Preprocess
-#python3 scripts/preprocessing.py --train "raw_datasets/gap_processed/dev.txt" \
-#              --valid "raw_datasets/gap_processed/valid.txt" \
-#              --test "raw_datasets/gap_processed/test.txt" \
-#              --unlabel "raw_datasets/gap_processed/dev.txt" \
-#              --dataset ${DATASET} \
-#              -o ${OUT}/
-#echo "loading pretrained vectors..."
-#python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
-#             --data_name "gap_processed" \
-#             --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
-#echo "preprocessing finished"
+python3 scripts/preprocessing.py --train "raw_datasets/gap_processed/dev.txt" \
+             --valid "raw_datasets/gap_processed/valid.txt" \
+             --test "raw_datasets/gap_processed/test.txt" \
+             --unlabel "raw_datasets/gap_processed/dev.txt" \
+             --dataset ${DATASET} \
+             -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+            --data_name "gap_processed" \
+            --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
+echo "preprocessing finished"
 export PYTHONPATH="$PWD"
 python3 scripts/main.py --data_folder=${OUT} \
               --dataset_name=${DATASET} \
@@ -32,7 +37,7 @@ python3 scripts/main.py --data_folder=${OUT} \
               --total_loss_weight=1 \
               --enable_logging \
               --use_CE \
-#              --use_AT \
-#              --use_EM \
-#              --use_VAT
+              --use_AT \
+              --use_EM \
+              --use_VAT
 
