cfb05cbe77b0381785a4258b0d7cf8227f1f031e
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2021-12-11 16:17:56 -0500

    Update train_dbpedia.sh

--- train_dbpedia.sh
+++ train_dbpedia.sh
@@ -3,12 +3,10 @@
 NAME="dbpedia_pretrained_mixed"
 OUT="temp/$NAME"
 DATASET="dbpedia"
-#'''
 #For Elec, AGNews and DBpedia, use lines below to download pretrained embeddings
-#'''
-#wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
-#unzip crawl-300d-2M.vec.zip
-#mv crawl-300d-2M.vec pretrained_embeddings/
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+unzip crawl-300d-2M.vec.zip
+mv crawl-300d-2M.vec pretrained_embeddings/
 
 #unzip tar.gz file
 tar -xvf raw_datasets/dbpedia/train.tar.gz
@@ -17,17 +15,17 @@ tar -xvf raw_datasets/dbpedia/test.tar.gz
 mv test.txt raw_datasets/dbpedia
 
 # Preprocess
-#python3 scripts/preprocessing.py --train "raw_datasets/dbpedia/train.txt" \
-#               --valid "raw_datasets/dbpedia/test.txt" \
-#               --test "raw_datasets/dbpedia/test.txt" \
-#               --unlabel "raw_datasets/dbpedia/train.txt" \
-#               --dataset ${DATASET} \
-#               -o ${OUT}/
-#echo "loading pretrained vectors..."
-#python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
-#             --data_name "dbpedia" \
-#             --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
-#echo "preprocessing finished"
+python3 scripts/preprocessing.py --train "raw_datasets/dbpedia/train.txt" \
+              --valid "raw_datasets/dbpedia/test.txt" \
+              --test "raw_datasets/dbpedia/test.txt" \
+              --unlabel "raw_datasets/dbpedia/train.txt" \
+              --dataset ${DATASET} \
+              -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+            --data_name "dbpedia" \
+            --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
+echo "preprocessing finished"
 
 export PYTHONPATH="$PWD"
 python3 scripts/main.py --data_folder=${OUT} \
@@ -40,6 +38,6 @@ python3 scripts/main.py --data_folder=${OUT} \
               --enable_logging \
               --logging_freq=10 \
               --use_CE \
-#              --use_AT \
-#              --use_EM \
-#              --use_VAT
+              --use_AT \
+              --use_EM \
+              --use_VAT
