695f329c081a64c0d02c04ffcc4a62c5a56dd37c
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2021-12-11 16:17:00 -0500

    Update train_agnews.sh

--- train_agnews.sh
+++ train_agnews.sh
@@ -3,25 +3,24 @@
 NAME="agnews_pretrained_mixed"
 OUT="temp/$NAME"
 DATASET="agnews"
-#'''
+
 #For Elec, AGNews and DBpedia, use lines below to download pretrained embeddings
-#'''
-#wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
-#unzip crawl-300d-2M.vec.zip
-#mv crawl-300d-2M.vec pretrained_embeddings/
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+unzip crawl-300d-2M.vec.zip
+mv crawl-300d-2M.vec pretrained_embeddings/
 
 # Preprocess
-#python3 scripts/preprocessing.py --train "raw_datasets/agnews/train.txt" \
-#               --valid "raw_datasets/agnews/test.txt" \
-#               --test "raw_datasets/agnews/test.txt" \
-#               --unlabel "raw_datasets/agnews/unlabel.txt" \
-#               --dataset ${DATASET} \
-#               -o ${OUT}/
-#echo "loading pretrained vectors..."
-#python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
-#             --data_name "agnews" \
-#             --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
-#echo "preprocessing finished"
+python3 scripts/preprocessing.py --train "raw_datasets/agnews/train.txt" \
+              --valid "raw_datasets/agnews/test.txt" \
+              --test "raw_datasets/agnews/test.txt" \
+              --unlabel "raw_datasets/agnews/unlabel.txt" \
+              --dataset ${DATASET} \
+              -o ${OUT}/
+echo "loading pretrained vectors..."
+python3 scripts/load_pretrained_embeddings.py --preprocessed_dir ${OUT} \
+            --data_name "agnews" \
+            --pretrained_embeddings "pretrained_embeddings/crawl-300d-2M.vec"
+echo "preprocessing finished"
 
 export PYTHONPATH="$PWD"
 python3 scripts/main.py --data_folder=${OUT} \
